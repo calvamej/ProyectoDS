9628aa6c8732025046da46510ce06f2ab7b63455
calvamej <cesaralvamejia@gmail.com>
2025-08-10 18:54:20 -0500

    Edit Scripts BD

--- db_venta.sql
+++ db_venta.sql
@@ -106,21 +106,3 @@ CREATE TABLE [dbo].[VentaDetalle](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[AuditLog] ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[AuditLog](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[UserEmail] [varchar](100),
-	[EntityName] [varchar](100) NOT NULL,
-	[Action] [varchar](100) NOT NULL,
-	[Timestamp] [datetime],
-	[Changes] [varchar](500) NOT NULL,
- CONSTRAINT [PK_AuditLog] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
--- db_venta_security.sql
+++ db_venta_security.sql
@@ -1,108 +1,38 @@
 USE [db_venta]
 GO
-/****** Object:  Table [dbo].[Categoria]    Script Date: 17/02/2024 15:52:34 ******/
+/****** Object:  Table [dbo].[AuditLog] ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Categoria](
-	[IdCategoria] [int] IDENTITY(1,1) NOT NULL,
-	[Nombre] [varchar](50) NULL,
- CONSTRAINT [PK_Categoria] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[AuditLog](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserEmail] [varchar](100),
+	[EntityName] [varchar](100) NOT NULL,
+	[Action] [varchar](100) NOT NULL,
+	[Timestamp] [datetime],
+	[Changes] [varchar](500) NOT NULL,
+ CONSTRAINT [PK_AuditLog] PRIMARY KEY CLUSTERED 
 (
-	[IdCategoria] ASC
+	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Cliente]    Script Date: 17/02/2024 15:52:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Cliente](
-	[IdCliente] [int] IDENTITY(1,1) NOT NULL,
-	[Nombre] [varchar](50) NOT NULL,
-	[Apellidos] [varchar](50) NOT NULL,
-	[DireccionEntrega] [varchar](150) NULL,
-	[Ciudad] [varchar](100) NULL,
- CONSTRAINT [PK_Cliente] PRIMARY KEY CLUSTERED 
-(
-	[IdCliente] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Pago]    Script Date: 17/02/2024 15:52:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Pago](
-	[IdPago] [int] IDENTITY(1,1) NOT NULL,
-	[IdVenta] [int] NOT NULL,
-	[Fecha] [datetime] NOT NULL,
-	[Monto] [decimal](18, 2) NOT NULL,
-	[FormaPago] [int] NOT NULL,
-	[NumeroTarjeta] [varchar](16) NULL,
-	[FechaVencimiento] [datetime] NULL,
-	[CVV] [varchar](3) NULL,
-	[NombreTitular] [varchar](100) NULL,
-	[NumeroCuotas] [int] NULL,
-	[Procesado] [int] NOT NULL,
- CONSTRAINT [PK_Pago] PRIMARY KEY CLUSTERED 
-(
-	[IdPago] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Producto]    Script Date: 17/02/2024 15:52:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Producto](
-	[IdProducto] [int] IDENTITY(1,1) NOT NULL,
-	[IdCategoria] [int] NOT NULL,
-	[Nombre] [varchar](50) NOT NULL,
-	[Stock] [int] NOT NULL,
-	[StockMinimo] [int] NOT NULL,
-	[PrecioUnitario] [decimal](18, 2) NOT NULL,
- CONSTRAINT [PK_Producto] PRIMARY KEY CLUSTERED 
-(
-	[IdProducto] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Venta]    Script Date: 17/02/2024 15:52:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Venta](
-	[IdVenta] [int] IDENTITY(1,1) NOT NULL,
-	[IdCliente] [int] NOT NULL,
-	[Fecha] [datetime] NOT NULL,
-	[Monto] [decimal](18, 2) NOT NULL,
- CONSTRAINT [PK_Venta] PRIMARY KEY CLUSTERED 
-(
-	[IdVenta] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[VentaDetalle]    Script Date: 17/02/2024 15:52:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[VentaDetalle](
-	[IdVentaDetalle] [int] IDENTITY(1,1) NOT NULL,
-	[IdVenta] [int] NOT NULL,
-	[IdProducto] [int] NOT NULL,
-	[Cantidad] [int] NOT NULL,
-	[Precio] [decimal](18, 2) NOT NULL,
-	[SubTotal] [decimal](18, 2) NOT NULL,
- CONSTRAINT [PK_VentaDetalle] PRIMARY KEY CLUSTERED 
-(
-	[IdVentaDetalle] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+
+/****** Auditoría por BD******/
+
+-- Trigger para detectar modificaciones no autorizadas
+CREATE TRIGGER tr_ProductTamperDetection
+ON Producto AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO AuditLog (
+        EntityName, Id, UserEmail, 
+        [Changes], Timestamp
+    )
+    SELECT 'Producto', i.Id, SYSTEM_USER,
+           'Old Values: ' + (SELECT * FROM deleted d WHERE d.Id = i.Id FOR JSON AUTO) + 
+		   'New Values: ' + (SELECT * FROM i FOR JSON AUTO),
+           GETDATE()
+    FROM inserted i
+END
